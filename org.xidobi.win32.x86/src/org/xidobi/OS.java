f77732f611e13555438ac81c6387e9f426745457
bressler <bressler@gemtec.eu>
2013-01-03 14:18:18 +0100

    initial commit

---
+++ org.xidobi.win32.x86/src/org/xidobi/OS.java
@@ -0,0 +1,54 @@
+/*
+ *   Created on: 03.01.2013
+ *      Authors: Christian Schwarz, Tobias Breﬂler
+ *
+ * Copyright 2013 Gemtec GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.xidobi;
+
+/**
+ * 
+ * 
+ * @author Christian Schwarz
+ * @author Tobias Breﬂler
+ */
+public class OS {
+
+	public final static int GENERIC_READ = 0x80000000;
+	public final static int GENERIC_WRITE = 0x40000000;
+
+	public final static int OPEN_EXISTING = 3;
+
+	public final static int FILE_FLAG_OVERLAPPED = 1073741824;
+	
+	public final static int INVALID_HANDLE_VALUE = -1;
+	
+	public final static int ERROR_ACCESS_DENIED = 5;
+	public final static int ERROR_FILE_NOT_FOUND = 2;
+
+	static {
+		System.loadLibrary("lib/xidobi_x86_win32");
+	}
+	
+	/**
+	 * See <a
+	 * href="http://msdn.microsoft.com/en-us/library/windows/desktop/aa363858(v=vs.85).aspx">http
+	 * ://msdn.microsoft.com/en-us/library/windows/desktop/aa363858(v=vs.85).aspx</a>
+	 */
+	public static native int CreateFile(String lpFileName, int dwDesiredAccess, int dwShareMode, int lpSecurityAttributes, int dwCreationDisposition, int dwFlagsAndAttributes, int hTemplateFile);
+
+	public static native boolean CloseHandle(int handle);
+	
+}
---
+++ org.xidobi.win32.x86/src/org/xidobi/Test.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright Gemtec GmbH 2009-2013
+ *
+ * Erstellt am: 03.01.2013 13:40:09
+ * Erstellt von: Tobias Breﬂler
+ */
+package org.xidobi;
+
+import static org.xidobi.OS.FILE_FLAG_OVERLAPPED;
+import static org.xidobi.OS.GENERIC_READ;
+import static org.xidobi.OS.GENERIC_WRITE;
+import static org.xidobi.OS.OPEN_EXISTING;
+
+/**
+ *
+ *
+ * @author Tobias Breﬂler
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int handle = OS.CreateFile("\\\\.\\COM1", 
+		              GENERIC_READ | GENERIC_WRITE, 
+		              0,
+		              0, 
+		              OPEN_EXISTING, 
+		              FILE_FLAG_OVERLAPPED, 
+		              0);
+		System.out.println("Handle: " + handle);
+		boolean status = OS.CloseHandle(handle);
+		System.out.println("Status: " + status);
+	}
+
+}
