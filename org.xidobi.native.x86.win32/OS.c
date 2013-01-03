e7e5bf72b36d1c0f2f14e0e35778d051f9c7d8d9
bressler <bressler@gemtec.eu>
2013-01-03 14:22:21 +0100

    initial commit of native library

---
+++ org.xidobi.native.x86.win32/OS.c
@@ -0,0 +1,57 @@
+/*
+ * OS.c
+ *
+ *   Created on: 03.01.2013
+ *      Authors: Christian Schwarz, Tobias Breﬂler
+ *
+ * Copyright 2013 Gemtec GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <jni.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "org_xidobi_OS.h"
+
+JNIEXPORT jint JNICALL
+Java_org_xidobi_OS_CreateFile(JNIEnv *env, jclass clazz, jstring lpFileName,
+		jint dwDesiredAccess, jint dwShareMode, jint lpSecurityAttributes,
+		jint dwCreationDisposition, jint dwFlagsAndAttributes,
+		jint hTemplateFile) {
+
+	const char* fileName = (*env)->GetStringUTFChars(env, lpFileName, NULL);
+
+	HANDLE handle;
+	handle = CreateFile(fileName,
+						dwDesiredAccess,
+						dwShareMode,
+						(LPSECURITY_ATTRIBUTES) lpSecurityAttributes,
+						dwCreationDisposition,
+						dwFlagsAndAttributes,
+						(HANDLE) hTemplateFile);
+
+	(*env)->ReleaseStringUTFChars(env, lpFileName, fileName);
+
+	return (jint) handle;
+}
+
+/*
+ * Class:     org_xidobi_OS
+ * Method:    CloseHandle
+ * Signature: (I)Z
+ */JNIEXPORT jboolean JNICALL
+Java_org_xidobi_OS_CloseHandle(JNIEnv *env, jclass clazz, jint handle) {
+	if (CloseHandle((HANDLE) handle))
+		return JNI_TRUE;
+	return JNI_FALSE;
+}
