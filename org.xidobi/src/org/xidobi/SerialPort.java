56a0fab070b244a1b65f0b4bd6afad4f3da777af
Christian Schwarz <chriss.dev@gmail.com>
2013-01-12 21:22:49 +0100

    Added JavaDocs

--- org.xidobi/src/org/xidobi/SerialPort.java
+++ org.xidobi/src/org/xidobi/SerialPort.java
@@ -3,8 +3,18 @@ package org.xidobi;
 import java.io.IOException;
 import java.util.concurrent.Executor;
 
+/**
+ * 
+ * @author Christian Schwarz
+ *
+ */
 public interface SerialPort {
 
+	/**
+	 * Writes the given byte[].
+	 * @param data must not be <code>null</code>
+	 * @throws IOException when the Port is closed
+	 */
 	void write(byte[] data) throws IOException;
 	
 	void setReceiver(Receiver receiver);
