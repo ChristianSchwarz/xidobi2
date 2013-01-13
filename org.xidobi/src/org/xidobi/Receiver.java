83e62a6ff5f8915266e4d9b935670164a7e09b4c
Christian Schwarz <chriss.dev@gmail.com>
2013-01-13 21:00:24 +0100

    JavaDoc added

--- org.xidobi/src/org/xidobi/Receiver.java
+++ org.xidobi/src/org/xidobi/Receiver.java
@@ -1,7 +1,23 @@
 package org.xidobi;
 
+import java.util.concurrent.Executor;
+
+/**
+ * The {@link Receiver} will be notified by the {@link SerialPort} to process
+ * received <code>byte's</code>.
+ * 
+ * @see SerialPort#setReceiver(Receiver)
+ * @see SerialPort#setReceiver(Receiver, Executor)
+ */
 public interface Receiver {
 
+	/**
+	 * This method will invoked if some data was received by the
+	 * {@link SerialPort}.
+	 * 
+	 * @param data
+	 *            the received bytes, never <code>null</code>
+	 */
 	void onDataReceived(byte[] data);
-	
+
 }
--- org.xidobi/src/org/xidobi/SerialPort.java
+++ org.xidobi/src/org/xidobi/SerialPort.java
@@ -6,17 +6,43 @@ import java.util.concurrent.Executor;
 /**
  * 
  * @author Christian Schwarz
- *
+ * 
  */
 public interface SerialPort {
 
 	/**
-	 * Writes the given byte[].
-	 * @param data must not be <code>null</code>
-	 * @throws IOException when the Port is closed
+	 * Writes the given byte[].All bytes of the array were written.
+	 * 
+	 * @param data
+	 *            must not be <code>null</code>
+	 * @throws IOException
+	 *             when the Port is closed
 	 */
 	void write(byte[] data) throws IOException;
-	
+
+	/**
+	 * Set the {@link Receiver} to be notified when data was received, or
+	 * <code>null</code> to remove the current {@link Receiver}.
+	 * 
+	 * @param receiver
+	 *            the {@link Receiver} to be notified or <code>null</code> to
+	 *            remove the current
+	 */
 	void setReceiver(Receiver receiver);
+
+	/**
+	 * Set the {@link Receiver} to be notified when data was received, or
+	 * <code>null</code> to remove the current {@link Receiver}. The given
+	 * {@link Executor} will be used to notify the receiver.
+	 * 
+	 * @param receiver
+	 *            the {@link Receiver} to be notified or <code>null</code> to
+	 *            remove the current {@link Receiver}
+	 * 
+	 * @param executor
+	 *            the {@link Executor} to be used to notifiy the <b>receiver</b>
+	 *            
+	 *            @exception IllegalArgumentException if <code>executor==null</code>
+	 */
 	void setReceiver(Receiver receiver, Executor executor);
 }
