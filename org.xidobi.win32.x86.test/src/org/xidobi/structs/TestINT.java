523e635c4853f97be869f993689f5d1ad39075f3
Tobias Breßler <bressler@gemtec.eu>
2013-02-22 10:59:51 +0100

    Added tests for equals-hashCode-contract in class INT.

--- org.xidobi.win32.x86.test/src/org/xidobi/structs/TestINT.java
+++ org.xidobi.win32.x86.test/src/org/xidobi/structs/TestINT.java
@@ -16,6 +16,7 @@
 package org.xidobi.structs;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
@@ -56,4 +57,84 @@ public class TestINT {
 		assertThat(obj.toString(), is("42"));
 	}
 
+	/**
+	 * Verifies the contract for the <code>equals()</code> and <code>hashCode()</code> methods. In
+	 * this case the hashcode for the two instances (with the same value) must be equal.
+	 */
+	@Test
+	public void equalsHashCodeContract_hashCodeWithSameValues() {
+		int same1 = new INT(12).hashCode();
+		int same2 = new INT(12).hashCode();
+
+		assertThat(same1, is(same2));
+	}
+
+	/**
+	 * Verifies the contract for the <code>equals()</code> and <code>hashCode()</code> methods. In
+	 * this case the hashcode for the two instances (with different values) must be unequal.
+	 */
+	@Test
+	public void equalsHashCodeContract_hashCodeWithDifferentValues() {
+		int instance = new INT(1).hashCode();
+		int unequal = new INT(2).hashCode();
+
+		assertThat(instance, is(not(unequal)));
+	}
+
+	/**
+	 * Verifies the contract for the <code>equals()</code> and <code>hashCode()</code> methods. In
+	 * this case the two instances with the same value must be equal.
+	 */
+	@Test
+	public void equalsHashCodeContract_equalWithSameValues() {
+		INT same1 = new INT(12);
+		INT same2 = new INT(12);
+
+		assertThat(same1.equals(same2), is(true));
+	}
+
+	/**
+	 * Verifies the contract for the <code>equals()</code> and <code>hashCode()</code> methods. In
+	 * this case the instance must be equal to itself.
+	 */
+	@Test
+	public void equalsHashCodeContract_equalWithSameInstance() {
+		INT instance = new INT(12);
+
+		assertThat(instance.equals(instance), is(true));
+	}
+
+	/**
+	 * Verifies the contract for the <code>equals()</code> and <code>hashCode()</code> methods. In
+	 * this case the instance must be unequal to <code>null</code>.
+	 */
+	@Test
+	public void equalsHashCodeContract_unequalWithNull() {
+		INT instance = new INT(12);
+
+		assertThat(instance.equals(null), is(false));
+	}
+
+	/**
+	 * Verifies the contract for the <code>equals()</code> and <code>hashCode()</code> methods. In
+	 * this case the instance must be unequal to another class.
+	 */
+	@Test
+	public void equalsHashCodeContract_unequalWithOtherClass() {
+		INT instance = new INT(12);
+
+		assertThat(instance.equals("other class"), is(false));
+	}
+
+	/**
+	 * Verifies the contract for the <code>equals()</code> and <code>hashCode()</code> methods. In
+	 * this case the two instance with different values must be unequal.
+	 */
+	@Test
+	public void equalsHashCodeContract_unequalWithDifferentValues() {
+		INT instance = new INT(1);
+		INT unequal = new INT(2);
+
+		assertThat(instance.equals(unequal), is(false));
+	}
 }
