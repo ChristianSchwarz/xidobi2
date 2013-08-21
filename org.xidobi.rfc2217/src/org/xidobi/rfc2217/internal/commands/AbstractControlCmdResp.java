4a8d5f7cf5f9be85ce20dc2813f90ffd4e304a92
Christian Schwarz <schwarz@gemtec.eu>
2013-08-21 16:07:45 +0200

    JavaDoc added

--- org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/commands/AbstractControlCmdResp.java
+++ org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/commands/AbstractControlCmdResp.java
@@ -9,23 +9,42 @@ package org.xidobi.rfc2217.internal.commands;
 import java.io.DataInput;
 import java.io.IOException;
 
+import javax.annotation.Nonnull;
+
 /**
- * Defines the Basic Interface for command that can be send to the access server or can be received
- * by the client.
+ * Base class for command responses send from the access server. Subclasses override
+ * {@link #read(DataInput)} to by the client.
  * 
  * @author Christian Schwarz
  * 
  */
 public abstract class AbstractControlCmdResp {
 
-	 AbstractControlCmdResp(DataInput input) throws IOException {
+	/**
+	 * This constructor is used by subclasses to decode the response.
+	 * <p>
+	 * Implementation Note: The the constuctor call will be delegate to {@link #read(DataInput)} to
+	 * {@link #read(DataInput)} in order to decode the content.
+	 * 
+	 * @param input
+	 *            used to decode the content
+	 * @throws IOException
+	 *             if the message is malformed or the underlying media can't be read
+	 */
+	AbstractControlCmdResp(@Nonnull DataInput input) throws IOException {
 		read(input);
 	}
 
 	/**
-	 * @param input
+	 * Subclasses implement this methode to decode the content of this instance. The given input
+	 * starts at the beginning of the response specific content. That means after the command
+	 * specific idenitfier.
+	 * 
+	 *@param input
+	 *            used to decode the content
+	 * @throws IOException
+	 *             if the message is malformed or the underlying media can't be read
 	 */
-	protected abstract void read(DataInput input)throws IOException;
+	protected abstract void read(@Nonnull DataInput input) throws IOException;
 
-	
 }
