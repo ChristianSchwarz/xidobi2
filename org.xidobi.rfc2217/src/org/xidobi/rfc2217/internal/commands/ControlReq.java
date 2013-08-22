589d847d0251d6d75203cf15a951f2e92df96885
Christian Schwarz <schwarz@gemtec.eu>
2013-08-22 08:43:06 +0200

    JavaDoc added

--- org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/commands/ControlReq.java
+++ org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/commands/ControlReq.java
@@ -6,16 +6,24 @@
  */
 package org.xidobi.rfc2217.internal.commands;
 
+import javax.annotation.Nonnull;
+
 /**
- * Base class for all Com-Port-Option Request Messages. The specific command is passed to the constructor.
+ * Base class for all Com-Port-Option Request Messages. The specific command is passed to the
+ * constructor.
+ * 
  * @author Christian Schwarz
- *
+ * 
  */
 public class ControlReq {
 
 	/**
+	 * Creates a new {@link ControlReq} with the given Command Request.
 	 * 
-	 * @param request
+	 * @param command
+	 *            the Command of this request
+	 * @exception IllegalArgumentException
+	 *                if <code>null</code> is passed
 	 */
-	public ControlReq(AbstractControlCmdReq request) {}
+	public ControlReq(@Nonnull AbstractControlCmdReq command) {}
 }
