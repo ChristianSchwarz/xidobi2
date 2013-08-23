fa9b46f8875d29cb140e9698536238e7ff85d869
nitsche <nitsche@FMA-NB30.Gemtec.local>
2013-08-23 15:11:53 +0200

    NAchrichtenobjekt Stopsize implementiert & getestet

---
+++ org.xidobi.rfc2217.test/src/org/xidobi/rfc2217/internal/commands/TestStopsizeControlCmd.java
@@ -0,0 +1,94 @@
+package org.xidobi.rfc2217.internal.commands;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.rules.ExpectedException.none;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+/**
+ * Tests the class {@link StopsizeControlCmd}.
+ * 
+ * @author Christin Nitsche
+ * 
+ */
+public class TestStopsizeControlCmd {
+	@Rule
+	public ExpectedException exception = none();
+
+	private StopsizeControlCmd cmd;
+
+	@Mock
+	private DataInput input;
+
+	@Before
+	public void setUp() throws IOException {
+		initMocks(this);
+		when(input.readByte()).thenReturn((byte) 2);
+
+		cmd = new StopsizeControlCmd(2);
+
+	}
+
+	/**
+	 * Checks whether the stopsize is read correctly.
+	 */
+	@Test
+	public void read_isCorrect() throws Exception {
+		cmd = new StopsizeControlCmd(input);
+		assertThat(cmd.getStopsize(), Matchers.is(2));
+	}
+
+	/**
+	 * When the stopsize is invalid, an {@link IOException} should be thrown.
+	 */
+	@Test
+	public void read_invalidStopsize() throws Exception {
+		exception.expect(IOException.class);
+		exception.expectMessage("The received stopsize is invalid! Expected a value greater or equal to 1, got: >-1<");
+
+		when(input.readByte()).thenReturn((byte) -1);
+		new StopsizeControlCmd(input);
+	}
+
+	/**
+	 * When a stopsize that is smaller than 1 is supplied to the constructor, an
+	 * {@link IllegalArgumentException} should be thrown.
+	 * 
+	 */
+	@Test
+	public void new_withNegativeStopsize() throws Exception {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("The stopsize must not be less than 1! Got: >-2<");
+		new StopsizeControlCmd(-2);
+	}
+
+
+
+	/**
+	 * Checks whether the encoded message is correct.
+	 */
+	@Test
+	public void write_correctData() throws Exception {
+		DataOutput output = Mockito.mock(DataOutput.class);
+		cmd.write(output);
+
+		InOrder order = Mockito.inOrder(output);
+
+		order.verify(output).write(44); 	// COM-PORT OPTION
+		order.verify(output).write(4); 		// SET_STOPSIZE
+		order.verify(output).writeByte(2); 	// The stopsize
+	}
+}
---
+++ org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/commands/StopsizeControlCmd.java
@@ -0,0 +1,84 @@
+package org.xidobi.rfc2217.internal.commands;
+
+import static org.xidobi.rfc2217.internal.RFC2217.COM_PORT_OPTION;
+import static org.xidobi.rfc2217.internal.RFC2217.SET_STOPSIZE;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+//@formatter:off
+
+/**
+ * 
+ * This command is sent by the client to the access server to set the number of stop bits. The
+ * command can also be sent to query the current stop bit size. The value is one octet (byte). The
+ * value is an index into the following value table:
+ * 
+ * 
+ * Value			Stop Bit Size 
+ * 0 				Request Current Data Size 
+ * 1				1
+ * 2				2
+ * 3				1.5
+ * 4-127			Available for Future Use
+ * 
+ * @author Christin Nitsche
+ * 
+ */
+//@formatter:on
+public class StopsizeControlCmd extends AbstractControlCmd {
+
+	/** The preferred stopsize. */
+	private int stopsize;
+
+	/**
+	 * Creates a new {@link StopsizeControlCmd}.
+	 * 
+	 * @param commandCode
+	 *            the preferred stopsize, greater or equal to one.
+	 */
+	StopsizeControlCmd(int stopsize) {
+		super(SET_STOPSIZE);
+		if (stopsize < 1)
+			throw new IllegalArgumentException("The stopsize must not be less than 1! Got: >" + stopsize + "<");
+		this.stopsize = stopsize;
+	}
+
+	/**
+	 * Creates a new {@link StopsizeControlCmd}.
+	 * 
+	 * @param input
+	 *            used to decode the content of the command, must not be <code>null</code>
+	 * @throws IOException
+	 *             if the message is malformed or the underlying media can't be read
+	 */
+	public StopsizeControlCmd(DataInput input) throws IOException {
+		super(SET_STOPSIZE, input);
+	}
+
+	@Override
+	protected void read(DataInput input) throws IOException {
+		int stopsize = input.readByte();
+		if (stopsize < 1)
+			throw new IOException("The received stopsize is invalid! Expected a value greater or equal to 1, got: >" + stopsize + "<");
+		this.stopsize = stopsize;
+	}
+
+	@Override
+	public void write(DataOutput output) throws IOException {
+		output.write(COM_PORT_OPTION);
+		output.write(SET_STOPSIZE);
+		output.writeByte(stopsize);
+	}
+
+	/**
+	 * Returns the preferred stopsize.
+	 * 
+	 * @return the stopsize, greater or equal to one
+	 */
+	public int getStopsize() {
+		return stopsize;
+	}
+
+}
