67850597cec63cedf706653638e2c1e00c5e27c3
Konrad Schulz <konrad.schulz@gemtec.eu>
2013-08-27 17:27:58 +0200

    Refactoring: StopbitsControlCmd, Tests added

--- org.xidobi.rfc2217.test/src/org/xidobi/rfc2217/internal/commands/TestStopBitsControlCmd.java
+++ org.xidobi.rfc2217.test/src/org/xidobi/rfc2217/internal/commands/TestStopBitsControlCmd.java
@@ -1,93 +1,143 @@
 package org.xidobi.rfc2217.internal.commands;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.rules.ExpectedException.none;
-
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import static org.mockito.MockitoAnnotations.initMocks;
+import static org.xidobi.StopBits.STOPBITS_1;
+import static org.xidobi.StopBits.STOPBITS_1_5;
+import static org.xidobi.StopBits.STOPBITS_2;
+import static testtools.MessageBuilder.buffer;
 
-import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.xidobi.StopBits;
 
 /**
  * Tests the class {@link StopBitsControlCmd}.
  * 
  * @author Christin Nitsche
- * 
+ * @author Konrad Schulz
  */
 public class TestStopBitsControlCmd {
+
 	@Rule
 	public ExpectedException exception = none();
 
 	private StopBitsControlCmd cmd;
 
 	@Mock
-	private DataInput input;
+	private DataOutput output;
 
 	@Before
+	@SuppressWarnings("javadoc")
 	public void setUp() throws IOException {
 		initMocks(this);
-		when(input.readByte()).thenReturn((byte) 2);
-
-		cmd = new StopBitsControlCmd(2);
-
 	}
 
 	/**
-	 * Checks whether the stopsize is read correctly.
+	 * When a <code>null</code> stopBit is supplied to the constructor, an
+	 * {@link IllegalArgumentException} must be thrown.
 	 */
+	@SuppressWarnings("unused")
 	@Test
-	public void read_isCorrect() throws Exception {
-		cmd = new StopBitsControlCmd(input);
-		assertThat(cmd.getStopBits(), Matchers.is(2));
-	}
-
-	/**
-	 * When the stopsize is invalid, an {@link IOException} should be thrown.
-	 */
-	@Test
-	public void read_invalidStopsize() throws Exception {
-		exception.expect(IOException.class);
-		exception.expectMessage("The received stopsize is invalid! Expected a value greater or equal to 1, got: >-1<");
-
-		when(input.readByte()).thenReturn((byte) -1);
-		new StopBitsControlCmd(input);
-	}
-
-	/**
-	 * When a stopsize that is smaller than 1 is supplied to the constructor, an
-	 * {@link IllegalArgumentException} should be thrown.
-	 * 
-	 */
-	@Test
-	public void new_withNegativeStopsize() throws Exception {
+	public void new_withNullStopBits() {
 		exception.expect(IllegalArgumentException.class);
-		exception.expectMessage("The stopsize must not be less than 1! Got: >-2<");
-		new StopBitsControlCmd(-2);
+		exception.expectMessage("The parameter >stopBits< must not be null");
+		new StopBitsControlCmd((StopBits) null);
 	}
 
-
-
 	/**
-	 * Checks whether the encoded message is correct.
+	 * Checks whether the stopBit is read correctly, {@link StopBits#STOPBITS_1}.
 	 */
 	@Test
-	public void write_correctData() throws Exception {
-		DataOutput output = Mockito.mock(DataOutput.class);
+	public void read_stopBits_1() throws IOException {
+		cmd = new StopBitsControlCmd(buffer(1).toDataInput());
+
+		assertThat(cmd.getStopBits(), is(STOPBITS_1));
+	}
+
+	/**
+	 * Checks whether the stopBit is read correctly, {@link StopBits#STOPBITS_1_5}.
+	 */
+	@Test
+	public void read_stopBits_1_5() throws IOException {
+		cmd = new StopBitsControlCmd(buffer(2).toDataInput());
+
+		assertThat(cmd.getStopBits(), is(STOPBITS_1_5));
+	}
+
+	/**
+	 * Checks whether the stopBit is read correctly, {@link StopBits#STOPBITS_2}.
+	 */
+	@Test
+	public void read_stopBits_2() throws IOException {
+		cmd = new StopBitsControlCmd(buffer(3).toDataInput());
+
+		assertThat(cmd.getStopBits(), is(STOPBITS_2));
+	}
+
+	/**
+	 * Checks whether the encoded message is correct, {@link StopBits#STOPBITS_1}.
+	 */
+	@Test
+	public void write_stopBits_1() throws IOException {
+		cmd = new StopBitsControlCmd(STOPBITS_1);
 		cmd.write(output);
 
-		verify(output).writeByte(2); 	// The stop bits
+		verify(output).writeByte(1);
+	}
+
+	/**
+	 * Checks whether the encoded message is correct, {@link StopBits#STOPBITS_1_5}.
+	 */
+	@Test
+	public void write_stopBits_1_5() throws IOException {
+		cmd = new StopBitsControlCmd(STOPBITS_1_5);
+		cmd.write(output);
+
+		verify(output).writeByte(2);
+	}
+
+	/**
+	 * Checks whether the encoded message is correct, {@link StopBits#STOPBITS_2}.
+	 */
+	@Test
+	public void write_stopBits_2() throws IOException {
+		cmd = new StopBitsControlCmd(STOPBITS_2);
+		cmd.write(output);
+
+		verify(output).writeByte(3);
+	}
+
+	/**
+	 * When the parity is invalid, an {@link IOException} should be thrown.
+	 */
+	@Test
+	@SuppressWarnings("unused")
+	public void read_invalidStopBits() throws IOException {
+		exception.expect(IOException.class);
+		exception.expectMessage("Unexpected stopBits value: 6");
+
+		new StopBitsControlCmd(buffer(6).toDataInput());
+	}
+
+	/**
+	 * When the parity is invalid, an {@link IOException} should be thrown.
+	 */
+	@Test
+	public void write_invalidStopBits() throws IOException {
+		exception.expect(IOException.class);
+		exception.expectMessage("Unexpected stopBits value: 6");
+
+		cmd = new StopBitsControlCmd(buffer(6).toDataInput());
+		cmd.write(output);
 	}
 }
--- org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/commands/StopBitsControlCmd.java
+++ org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/commands/StopBitsControlCmd.java
@@ -1,13 +1,15 @@
 package org.xidobi.rfc2217.internal.commands;
 
-import static org.xidobi.rfc2217.internal.RFC2217.COM_PORT_OPTION;
+import static org.xidobi.StopBits.STOPBITS_1;
+import static org.xidobi.StopBits.STOPBITS_1_5;
+import static org.xidobi.StopBits.STOPBITS_2;
 import static org.xidobi.rfc2217.internal.RFC2217.SET_STOPSIZE_REQ;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-
+import org.xidobi.StopBits;
 
 /**
  * 
@@ -23,13 +25,14 @@ import java.io.IOException;
  *     3		1.5
  * 4-127		Available for Future Use
  * </pre>
- * @author Christin Nitsche
  * 
+ * @author Christin Nitsche
+ * @author Konrad Schulz
  */
 
 public class StopBitsControlCmd extends AbstractControlCmd {
 
-	private int stopBits;
+	private StopBits stopBits;
 
 	/**
 	 * Creates a new {@link StopBitsControlCmd}.
@@ -37,11 +40,11 @@ public class StopBitsControlCmd extends AbstractControlCmd {
 	 * @param commandCode
 	 *            the preferred stopsize, greater or equal to one.
 	 */
-	StopBitsControlCmd(int stopsize) {
+	StopBitsControlCmd(StopBits stopBits) {
 		super(SET_STOPSIZE_REQ);
-		if (stopsize < 1)
-			throw new IllegalArgumentException("The stopsize must not be less than 1! Got: >" + stopsize + "<");
-		this.stopBits = stopsize;
+		if (stopBits == null)
+			throw new IllegalArgumentException("The parameter >stopBits< must not be null");
+		this.stopBits = stopBits;
 	}
 
 	/**
@@ -58,25 +61,69 @@ public class StopBitsControlCmd extends AbstractControlCmd {
 
 	@Override
 	protected void read(DataInput input) throws IOException {
-		int stopBits = input.readByte();
-		if (stopBits < 1)
-			throw new IOException("The received stopsize is invalid! Expected a value greater or equal to 1, got: >" + stopBits + "<");
+		byte byteValue = input.readByte();
+		StopBits stopBits = toEnum(byteValue);
 		this.stopBits = stopBits;
 	}
 
 	@Override
 	public void write(DataOutput output) throws IOException {
-
-		output.writeByte(stopBits);
+		output.writeByte(toByte(stopBits));
 	}
 
 	/**
-	 * Returns the preferred stopsize.
+	 * Returns the preferred stop bits.
 	 * 
-	 * @return the stopsize, greater or equal to one
+	 * @return the stop bits
 	 */
-	public int getStopBits() {
+	public StopBits getStopBits() {
 		return stopBits;
+	}
+
+	private StopBits toEnum(final byte stopBits) throws IOException {
+		switch (stopBits) {
+			case 1:
+				return STOPBITS_1;
+			case 2:
+				return STOPBITS_1_5;
+			case 3:
+				return STOPBITS_2;
+		}
+		throw new IOException("Unexpected stopBits value: " + stopBits);
+	}
+
+	private int toByte(StopBits stopBits) {
+		switch (stopBits) {
+			case STOPBITS_1:
+				return 1;
+			case STOPBITS_1_5:
+				return 2;
+			case STOPBITS_2:
+				return 3;
+		}
+		throw new IllegalStateException("Unexpected stopBits value:" + stopBits);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((stopBits == null) ? 0 : stopBits.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StopBitsControlCmd other = (StopBitsControlCmd) obj;
+		if (stopBits != other.stopBits)
+			return false;
+		return true;
 	}
 
 }
