4bcb23a56d465bd89c2df43dd682499fd96df33e
Konrad Schulz <konrad.schulz@gemtec.eu>
2013-08-28 09:21:46 +0200

    added Tests to TestDataBitsControlCmd

--- org.xidobi.rfc2217.test/src/org/xidobi/rfc2217/internal/commands/TestDataBitsControlCmd.java
+++ org.xidobi.rfc2217.test/src/org/xidobi/rfc2217/internal/commands/TestDataBitsControlCmd.java
@@ -6,7 +6,17 @@
  */
 package org.xidobi.rfc2217.internal.commands;
 
-import java.io.DataInput;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.xidobi.DataBits.DATABITS_5;
+import static org.xidobi.DataBits.DATABITS_6;
+import static org.xidobi.DataBits.DATABITS_7;
+import static org.xidobi.DataBits.DATABITS_8;
+import static org.xidobi.DataBits.DATABITS_9;
+import static testtools.MessageBuilder.buffer;
+
 import java.io.DataOutput;
 import java.io.IOException;
 
@@ -16,14 +26,6 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.xidobi.DataBits;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.xidobi.DataBits.DATABITS_5;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-import static testtools.MessageBuilder.buffer;
 
 /**
  * Tests the class {@link DataBitsControlCmd}.
@@ -38,7 +40,7 @@ public class TestDataBitsControlCmd {
 	private DataBitsControlCmd cmd;
 
 	@Mock
-	private DataInput input;
+	private DataOutput output;
 
 	@Before
 	@SuppressWarnings("javadoc")
@@ -47,7 +49,7 @@ public class TestDataBitsControlCmd {
 	}
 
 	/**
-	 * When a <code>null</code> datasize is supplied to the constructor, an
+	 * When a <code>null</code> dataBits is supplied to the constructor, an
 	 * {@link IllegalArgumentException} must be thrown.
 	 */
 	@SuppressWarnings("unused")
@@ -57,17 +59,107 @@ public class TestDataBitsControlCmd {
 	}
 
 	/**
-	 * Checks whether the dataSize is read correctly.
+	 * Checks whether the dataBits is read correctly.
 	 */
 	@Test
-	public void read_isCorrect() throws IOException {
+	public void read_dataBits_5() throws IOException {
 		cmd = new DataBitsControlCmd(buffer(5).toDataInput());
 
 		assertThat(cmd.getDataBits(), is(DATABITS_5));
 	}
 
 	/**
-	 * When the dataSize is invalid, an {@link IOException} must be thrown.
+	 * Checks whether the dataBits is read correctly, {@link DataBits#DATABITS_6}.
+	 */
+	@Test
+	public void read_dataBits_6() throws IOException {
+		cmd = new DataBitsControlCmd(buffer(6).toDataInput());
+
+		assertThat(cmd.getDataBits(), is(DATABITS_6));
+	}
+
+	/**
+	 * Checks whether the dataBits is read correctly, {@link DataBits#DATABITS_7}.
+	 */
+	@Test
+	public void read_dataBits_7() throws IOException {
+		cmd = new DataBitsControlCmd(buffer(7).toDataInput());
+
+		assertThat(cmd.getDataBits(), is(DATABITS_7));
+	}
+
+	/**
+	 * Checks whether the dataBits is read correctly, {@link DataBits#DATABITS_8}.
+	 */
+	@Test
+	public void read_dataBits_8() throws IOException {
+		cmd = new DataBitsControlCmd(buffer(8).toDataInput());
+
+		assertThat(cmd.getDataBits(), is(DATABITS_8));
+	}
+
+	/**
+	 * Checks whether the dataBits is read correctly, {@link DataBits#DATABITS_9}.
+	 */
+	@Test
+	public void read_dataBits_9() throws IOException {
+		cmd = new DataBitsControlCmd(buffer(9).toDataInput());
+
+		assertThat(cmd.getDataBits(), is(DATABITS_9));
+	}
+
+	/**
+	 * Checks whether the encoded message is correct, {@link DataBits#DATABITS_5}.
+	 */
+	@Test
+	public void write_dataBits_5() throws IOException {
+		new DataBitsControlCmd(DATABITS_5).write(output);
+
+		verify(output).writeByte(5); // The dataBits
+	}
+
+	/**
+	 * Checks whether the encoded message is correct, {@link DataBits#DATABITS_6}.
+	 */
+	@Test
+	public void write_dataBits_6() throws IOException {
+		new DataBitsControlCmd(DATABITS_6).write(output);
+
+		verify(output).writeByte(6); // The dataBits
+	}
+
+	/**
+	 * Checks whether the encoded message is correct, {@link DataBits#DATABITS_7}.
+	 */
+	@Test
+	public void write_dataBits_7() throws IOException {
+		new DataBitsControlCmd(DATABITS_7).write(output);
+
+		verify(output).writeByte(7); // The dataBits
+	}
+
+	/**
+	 * Checks whether the encoded message is correct, {@link DataBits#DATABITS_8}.
+	 */
+	@Test
+	public void write_dataBits_8() throws IOException {
+		new DataBitsControlCmd(DATABITS_8).write(output);
+
+		verify(output).writeByte(8); // The dataBits
+	}
+
+	/**
+	 * Checks whether the encoded message is correct, {@link DataBits#DATABITS_9}.
+	 */
+	@Test
+	public void write_dataBits_9() throws IOException {
+		new DataBitsControlCmd(DATABITS_9).write(output);
+
+		verify(output).writeByte(9); // The dataBits
+	}
+
+	/**
+	 * When the dataBits is invalid, an {@link IOException} must be thrown.
 	 */
 	@Test
 	public void read_invalidDataBits() throws IOException {
@@ -78,14 +170,14 @@ public class TestDataBitsControlCmd {
 	}
 
 	/**
-	 * Checks whether the encoded message is correct.
+	 * When the dataBits is invalid, an {@link IOException} must be thrown.
 	 */
 	@Test
-	public void write_correctData() throws IOException {
-		DataOutput output = mock(DataOutput.class);
-		new DataBitsControlCmd(DATABITS_5).write(output);
+	public void write_invalidParity() throws IOException {
+		exception.expect(IOException.class);
+		exception.expectMessage("Unexpected DataBits value: 4");
 
-		verify(output).writeByte(5); // The datasize
+		cmd = new DataBitsControlCmd(buffer(4).toDataInput());
+		cmd.write(output);
 	}
-
 }
