521251890be3a9be2f0407f521cafd22b7048f0c
Peter-René Jeschke <jeschke@gemtec.eu>
2013-09-02 10:19:03 +0200

    Implemented Writer

--- org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/SerialConnectionImpl.java
+++ org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/SerialConnectionImpl.java
@@ -25,7 +25,7 @@ public class SerialConnectionImpl extends BasicSerialConnection {
 	 */
 	public SerialConnectionImpl(@Nonnull Rfc2217SerialPort parent,
 								@Nonnull TelnetClient telnetClient) {
-		super(parent, new ReaderImpl(telnetClient.getInputStream()), new WriterImpl());
+		super(parent, new ReaderImpl(telnetClient.getInputStream()), new WriterImpl(telnetClient.getOutputStream()));
 	}
 
 }
--- org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/WriterImpl.java
+++ org.xidobi.rfc2217/src/org/xidobi/rfc2217/internal/WriterImpl.java
@@ -6,18 +6,36 @@
  */
 package org.xidobi.rfc2217.internal;
 
+import static org.xidobi.spi.Preconditions.checkArgumentNotNull;
+
 import java.io.IOException;
+import java.io.OutputStream;
 
 import org.xidobi.spi.Writer;
 
 /**
  * @author Christian Schwarz
- *
  */
+@SuppressWarnings("restriction")
 final class WriterImpl implements Writer {
-	public void close() throws IOException {}
 
-	public void dispose() {}
+	private OutputStream outputStream;
 
-	public void write(byte[] data) throws IOException {}
+	/**
+	 * @param outputStream
+	 */
+	public WriterImpl(OutputStream outputStream) {
+		this.outputStream = checkArgumentNotNull(outputStream, "outputStream");
+
+	}
+
+	public void close() throws IOException {
+	}
+
+	public void dispose() {
+	}
+
+	public void write(byte[] data) throws IOException {
+		outputStream.write(data);
+	}
 }
